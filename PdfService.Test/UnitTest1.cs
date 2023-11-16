2b51d9ae8005ff6f656d7c6e97bde7b44f4a873d
M-Busk <125284318+M-Busk@users.noreply.github.com>
2023-11-16 14:15:29 +0100

    initial commit

---
+++ PdfService.Test/UnitTest1.cs
@@ -0,0 +1,126 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace PdfService.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        protected WebApplicationFactory<Program> ApplicationFactory { get; init; }
+
+        public UnitTest1()
+        {
+            ApplicationFactory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                //builder.UseEnvironment("Test");
+            });
+        }
+
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            HttpClient client = ApplicationFactory.CreateClient();
+            Dictionary<string, string> myDict = new();
+            myDict.Add("Test1", "Test2");
+            byte[]? result = await client.PostJsonAsync<Dictionary<string, string>, byte[]>("/PdfProcessor/PdfContract", myDict);
+            if (result is not null)
+            {
+                System.IO.File.WriteAllBytes(@"C:\\Users\mbuskies\Desktop\myfile.pdf", result);
+            }
+            Assert.IsNotNull(result);
+        }
+    }
+
+    public static class HttpClientExtensions
+    {
+        private static JsonSerializerOptions TrainCloudJsonSerializerOptions { get; set; } = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+        public static async Task<TResponse?> GetJsonAsync<TResponse>(this HttpClient client, string requestUri)
+        {
+            try
+            {
+                using HttpResponseMessage response = await client.GetAsync(requestUri);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return default;
+                }
+
+                Stream contentStream = await response.Content.ReadAsStreamAsync();
+                TResponse? model = await JsonSerializer.DeserializeAsync<TResponse>(contentStream, TrainCloudJsonSerializerOptions);
+
+                return model;
+            }
+            catch (Exception ex)
+            {
+                return default;
+            }
+        }
+
+        public static async Task<TResponse?> PostJsonAsync<TPost, TResponse>(this HttpClient client, string requestUri, TPost param)
+        {
+            try
+            {
+                HttpContent parameterContent = JsonContent.Create(param, typeof(TPost));
+
+                using HttpResponseMessage response = await client.PostAsync(requestUri, parameterContent);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return default;
+                }
+
+
+                Stream contentStream = await response.Content.ReadAsStreamAsync();
+                TResponse? model = await JsonSerializer.DeserializeAsync<TResponse>(contentStream, TrainCloudJsonSerializerOptions);
+
+                return model;
+            }
+            catch (Exception ex)
+            {
+                return default;
+            }
+        }
+
+        public static async Task<TResponse?> PatchJsonAsync<TPatch, TResponse>(this HttpClient client, string requestUri, TPatch param)
+        {
+            try
+            {
+                HttpContent parameterContent = JsonContent.Create(param, typeof(TPatch));
+
+                using HttpResponseMessage response = await client.PatchAsync(requestUri, parameterContent);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return default;
+                }
+
+                Stream contentStream = await response.Content.ReadAsStreamAsync();
+                TResponse? model = await JsonSerializer.DeserializeAsync<TResponse>(contentStream, TrainCloudJsonSerializerOptions);
+
+                return model;
+            }
+            catch (Exception ex)
+            {
+                return default;
+            }
+        }
+
+        public static async Task<bool> DeleteAsyncX(this HttpClient client, string requestUri)
+        {
+            try
+            {
+                using HttpResponseMessage response = await client.DeleteAsync(requestUri);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PdfService/Controllers/PdfProcessorController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Diagnostics;
+
+namespace PdfService.Controllers;
+
+[ApiController]
+[Route("/PdfProcessor/")]
+public class PdfProcessorController : ControllerBase
+{
+    protected IWebHostEnvironment WebHostEnvironment { get; init; }
+
+    public PdfProcessorController(IWebHostEnvironment webHostEnvironment) 
+    {
+        WebHostEnvironment = webHostEnvironment;
+    }
+
+    [HttpPost("PdfContract")]
+    [Produces("application/pdf")]
+    [Consumes("application/json")]
+    [SwaggerResponse(StatusCodes.Status200OK)]
+    [SwaggerResponse(StatusCodes.Status400BadRequest)]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+    public IActionResult PostPdfContract([FromBody] Dictionary<string, string> data)
+    {
+        try
+        {
+            Document document = Document.Create(document =>
+                    {
+                        document.Page(page =>
+                        {
+                            page.Margin(1, Unit.Inch);
+
+                            page.Header()
+                                .Text("Hello PDF")
+                                .FontSize(48)
+                                .FontColor(Colors.Blue.Darken2)
+                                .SemiBold();
+
+                            page.Content()
+                                .Background(Colors.Grey.Medium);
+
+                            page.Content().Column(column =>
+                            {
+                                column.Item().Text(Placeholders.LoremIpsum()).FontSize(18);
+                            });
+                        });
+                    });
+
+            byte[] myDocByteArray = document.GeneratePdf();
+
+            return Ok(myDocByteArray);
+        } 
+        catch(Exception ex)
+        {
+            Debugger.Break();
+            return InternalServerError(ex);
+        }
+    }
+
+    protected ActionResult InternalServerError(Exception ex)
+    {
+        if (WebHostEnvironment.IsDevelopment())
+        {
+            return StatusCode(500, ex.Message);
+        }
+
+        return StatusCode(500);
+    }
+}
