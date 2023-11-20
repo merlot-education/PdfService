3717426093589f48091293ea0ee1002943cd5b8a
M-Busk <125284318+M-Busk@users.noreply.github.com>
2023-11-20 09:53:45 +0100

    update test

--- PdfService.Test/UnitTest_PdfProcessor.cs
+++ PdfService.Test/UnitTest_PdfProcessor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using PdfService.Models;
 
 namespace PdfService.Test;
 
@@ -19,9 +20,36 @@ public class UnitTest_PdfProcessor
     public async Task GenerateContractPdf()
     {
         HttpClient client = ApplicationFactory.CreateClient();
-        Dictionary<string, string> myDict = new();
-        myDict.Add("Test1", "Test2");
-        byte[]? result = await client.PostJsonAsync<Dictionary<string, string>, byte[]>("/PdfProcessor/PdfContract", myDict);
+        var model = new ContractModel()
+        {
+            ContractId = "Contract:1357",
+            ContractCreationDate = "01.02.2023",
+            ContractRuntime = "5 Tage",
+            ContractDataTransferCount = "10",
+            ContractAttachmentFilenames = ["MeineTolleDatei.pdf", "MeineAndereTolleDatei.pdf"],
+            ContractTnc = [
+                new ContractTncModel() { TncLink = "http://example.com", TncHash = "hash1234" },
+                new ContractTncModel() { TncLink = "http://merlot-education.eu", TncHash = "hash1357" }
+            ],
+
+            ServiceId = "ServiceOffering:1234",
+            ServiceName = "Mein Dienst",
+            ServiceType = "Datenlieferung",
+            ServiceDescription = "Liefert Daten von A nach B",
+            ServiceDataAccessType = "Download",
+            ServiceHardwareRequirements = "Flux-Kondensator mit 1.21 Gigawatt",
+
+            ProviderLegalName = "MeinAnbieter GmbH",
+            ProviderSignerUser = "Hans Wurst",
+            ProviderSignature = "12345678",
+            ProviderSignatureTimestamp = "01.02.2023 10:05",
+
+            ConsumerLegalName = "Konsum AG",
+            ConsumerSignerUser = "Marco Polo",
+            ConsumerSignature = "87654321",
+            ConsumerSignatureTimestamp = "01.02.2023 09:45"
+        };
+        byte[]? result = await client.PostJsonAsync<ContractModel, byte[]>("/PdfProcessor/PdfContract", model);
         Assert.IsNotNull(result);
     }
 }
